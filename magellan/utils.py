7311f00b2df484ec069794dc4afc7b400560f675
Sam Skjonsberg <sams@allenai.org>
2020-03-19 20:33:58 -0700

    Simplify doc for the stackdriver formatter.

--- magellan/utils.py
+++ magellan/utils.py
@@ -2,14 +2,7 @@ from pythonjsonlogger import jsonlogger
 
 class StackdriverJsonFormatter(jsonlogger.JsonFormatter):
     """
-    Custom log JSON log formatter that adds the severity member, allowing
-    end users to filter logs by the level of the log message emitted.
-
-    TODO: Parse request logs and add fields for each request element (user-agent
-    processing time, etc)
-
-    TODO:Add a timestamp that's used in place of Stackdriver's records (which
-    reflect the time the log was written to Stackdriver, I think).
+    JSON log formatter that's Google Stackdriver friendly.
     """
     def add_fields(self, log_record, record, message_dict):
         super(StackdriverJsonFormatter, self).add_fields(log_record, record,
